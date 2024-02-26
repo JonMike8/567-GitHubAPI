11f25ad8279faa81a6d2f0ab0f2fed3767ee6069
Jonathan Memoli <jonomemoli3@gmail.com>
2024-02-26 14:12:41 -0500

    Reverto to python 3.5, removed f-strings to avoid syntax error

--- githubAPI.py
+++ githubAPI.py
@@ -1,30 +1,27 @@
 import requests
-#import json
 
 def getUserHistory(user_id):
     if not isinstance(user_id, str):
         print("Input must be a string")
         return []
-    user_id.strip
+    user_id.strip()
 
-    url = f"https://api.github.com/users/{user_id}/repos"
+    url = "https://api.github.com/users/{}/repos".format(user_id)
     response = requests.get(url)
     if response.status_code != 200:
         print("Can't fetch user data")
         return []        
     repos = response.json()
-    #print(repos)
     commitsCount = []
 
     for repo in repos:
         repoName = repo['name']
-        commitUrl = f"https://api.github.com/repos/{user_id}/{repoName}/commits"
+        commitUrl = "https://api.github.com/repos/{}/{}/commits".format(user_id, repoName)
         commitResponse = requests.get(commitUrl)
-        if(response.status_code != 200):
+        if(commitResponse.status_code != 200):
             print("Can't fetch user commit data")
             return []
         commits = commitResponse.json()
         numCommits = len(commits)
         commitsCount.append((repoName, numCommits))
     return commitsCount
-
